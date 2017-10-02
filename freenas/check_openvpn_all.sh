6c35010261a6490a10de970f12f3e923c7308f32
jdarr <jdarr@bastion.solarsquid.local>
2017-10-01 19:34:31 -0700

    Removing files

--- freenas/check_openvpn_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read id name; do
-    echo "${name}"
-    echo "-------------------------------"
-    jexec ${id} "/root/check_openvpn.sh"
-    echo "-------------------------------"
-done < <(jls | grep transmission | awk '{ print $1" "$3 }')
