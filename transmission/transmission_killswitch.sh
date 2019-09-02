7db7157f84762c7fc8338ce20d0a0372696fd0ba
jmdarr <jeremy.darr@gmail.com>
2019-09-02 12:41:46 -0700

    Updated

--- transmission/transmission_killswitch.sh
+++ transmission/transmission_killswitch.sh
@@ -1,19 +1,19 @@
 #!/bin/env bash
 
-ip=$(/bin/curl -s icanhazip.com)
+ip=$(/bin/curl -s ifconfig.co)
 echo "VPN IP Lookup: ${ip}"
 output=$(/bin/geoiplookup "${ip}")
 echo "VPN Location: ${output}"
 [[ "${output}" =~ "Canada" ]] && {
-  output=$(ps aux | grep -i transmission | grep -v grep)
-  [ "x${output}" == "x" ] && {
+  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
+  [ ${output} -eq 0 ] && {
     echo "Did not find running transmission-daemon, starting"
     systemctl start transmission-daemon;
     logger "Starting transmission as openvpn is running just fine";
   } || { echo "Transmission running, keep on keeping on"; }
 } || {
-  output=$(ps aux | grep -i transmission | grep -v grep)
-  [ "x${output}" != "x" ] && {
+  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
+  [ ${output} -ne 0 ] && {
     echo "Found running transmission-daemon, stopping"
     systemctl stop transmission-daemon;
     logger openvpn-killswitch "Stopping transmission as openvpn is not running right";
