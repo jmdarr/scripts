c27e75157e15cbf0c84295e37317c66200dac6c1
jmdarr <jeremy.darr@gmail.com>
2020-03-30 09:03:04 -0700

    changing curl for dig

--- transmission/transmission_killswitch.sh
+++ transmission/transmission_killswitch.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-ip=$(/bin/curl -s ifconfig.co)
+ip=$(dig @resolver1.opendns.com ANY myip.opendns.com +short) || { echo "Could not get IP, exiting."; exit 1; }
 echo "VPN IP Lookup: ${ip}"
 output=$(/bin/geoiplookup "${ip}")
 echo "VPN Location: ${output}"
@@ -9,13 +9,13 @@ echo "VPN Location: ${output}"
   [ ${output} -eq 0 ] && {
     echo "Did not find running transmission-daemon, starting"
     systemctl start transmission-daemon;
-    logger "Starting transmission as openvpn is running just fine";
+    logger transmission-killswitch "Starting transmission as openvpn is running just fine";
   } || { echo "Transmission running, keep on keeping on"; }
 } || {
   output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
   [ ${output} -ne 0 ] && {
     echo "Found running transmission-daemon, stopping"
     systemctl stop transmission-daemon;
-    logger openvpn-killswitch "Stopping transmission as openvpn is not running right";
+    logger transmission-killswitch "Stopping transmission as openvpn is not running right";
   } || { echo "Transmission dead, all is well"; }
 }
