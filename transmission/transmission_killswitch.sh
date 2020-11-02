ca36e2f58758bb74106a842f878f0c37a643a080
jmdarr <jeremy.darr@gmail.com>
2020-11-02 15:15:17 -0700

    Adding more error checking

--- transmission/transmission_killswitch.sh
+++ transmission/transmission_killswitch.sh
@@ -1,10 +1,17 @@
 #!/bin/env bash
 
+# check for IP via dns resolution
 ip=$(dig @resolver1.opendns.com myip.opendns.com +short) || { echo "Could not get IP, exiting."; exit 1; }
 [ "${ip}x" == "x" ] && { echo "Could not get IP, exiting."; exit 1; }
 echo "VPN IP Lookup: ${ip}"
+
+# locate where our IP is, geographically speaking
 output=$(/bin/geoiplookup "${ip}")
+[ "${output}x" == "x" ] && { echo "Unable to geoiplookup '${ip}'."; exit 1; }
 echo "VPN Location: ${output}"
+
+# check to make sure we're resolving from Canadia... for reasons
+# TODO: make this more readable
 [[ "${output}" =~ "Canada" ]] && {
   output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
   [ ${output} -eq 0 ] && {
