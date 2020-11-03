8dc719e00eb1c6b14e6c0a5c5b18e7a01fe9e8c8
jmdarr <jeremy.darr@gmail.com>
2020-11-02 20:29:53 -0700

    adding proper messages to beginning of script

--- transmission/transmission_killswitch.sh
+++ transmission/transmission_killswitch.sh
@@ -1,29 +1,31 @@
 #!/bin/env bash
 
 # check for IP via dns resolution
-ip=$(dig @resolver1.opendns.com myip.opendns.com +short) || { echo "Could not get IP, exiting."; exit 1; }
-[ "${ip}x" == "x" ] && { echo "Could not get IP, exiting."; exit 1; }
-echo "VPN IP Lookup: ${ip}"
+echo -n 'Checking for VPN IP... '
+ip=$(dig @resolver1.opendns.com myip.opendns.com +short) || { echo "dig command failed, exiting."; exit 1; }
+[ "${ip}x" == "x" ] && { echo "IP lookup returned empty, exiting."; exit 1; }
+echo "found: ${ip}"
 
 # locate where our IP is, geographically speaking
-output=$(/bin/geoiplookup "${ip}")
-[ "${output}x" == "x" ] && { echo "Unable to geoiplookup '${ip}'."; exit 1; }
-echo "VPN Location: ${output}"
+echo -n 'Checking for GeoIP location... '
+output=$(/bin/geoiplookup "${ip}") || { echo 'geoiplookup command failed, exiting.'; exit 1; }
+[ "${output}x" == "x" ] && { echo "geoiplookup returned empty, exiting."; exit 1; }
+echo "found: ${output}"
 
 # check to make sure we're resolving from Canadia... for reasons
 # TODO: make this more readable
-[[ "${output}" =~ "Canada" ]] && {
-  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
-  [ ${output} -eq 0 ] && {
-    echo "Did not find running transmission-daemon, starting"
-    systemctl start transmission-daemon;
-    logger transmission-killswitch "Starting transmission as openvpn is running just fine";
-  } || { echo "Transmission running, keep on keeping on"; }
-} || {
-  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
-  [ ${output} -ne 0 ] && {
-    echo "Found running transmission-daemon, stopping"
-    systemctl stop transmission-daemon;
-    logger transmission-killswitch "Stopping transmission as openvpn is not running right";
-  } || { echo "Transmission dead, all is well"; }
-}
+#[[ "${output}" =~ "Canada" ]] && {
+#  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
+#  [ ${output} -eq 0 ] && {
+#    echo "Did not find running transmission-daemon, starting"
+#    systemctl start transmission-daemon;
+#    logger transmission-killswitch "Starting transmission as openvpn is running just fine";
+#  } || { echo "Transmission running, keep on keeping on"; }
+#} || {
+#  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
+#  [ ${output} -ne 0 ] && {
+#    echo "Found running transmission-daemon, stopping"
+#    systemctl stop transmission-daemon;
+#    logger transmission-killswitch "Stopping transmission as openvpn is not running right";
+#  } || { echo "Transmission dead, all is well"; }
+#}
