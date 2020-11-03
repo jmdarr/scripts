de19f672df2221be99a5079e9eae6dc57d12cbb7
jmdarr <jeremy.darr@gmail.com>
2020-11-02 20:49:46 -0700

    A very large rewrite. Bad llama (commits should be smaller)

--- transmission/transmission_killswitch.sh
+++ transmission/transmission_killswitch.sh
@@ -1,5 +1,51 @@
 #!/bin/env bash
 
+wanted_country='Canada'
+svc='transmission-daemon'
+
+function check_for_running_transmission() {
+  pscount=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
+  rval=1
+  if [ ${pscount} -gt 0 ]; then rval=0; fi
+  return ${rval}
+}
+
+function start_transmission() {
+  rval=-1
+  echo -n 'Starting transmission... '
+  if check_for_running_transmission; then
+    echo 'already running.'
+    rval=1
+  else
+    if systemctl start ${svc} >/dev/null 2>&1; then
+      echo 'started.'
+      rval=0
+    else
+      echo 'failed.'
+      rval=1
+    fi
+  fi
+  return ${rval}
+}
+
+function stop_transmission() {
+  rval=-1
+  echo -n 'Stopping transmission... '
+  if check_for_running_transmission; then
+    if systemctl stop ${svc} >/dev/null 2>&1; then
+      echo 'stopped.'
+      rval=0
+    else
+      echo 'failed.'
+      rval=1
+    fi
+  else
+    echo 'already stopped.'
+    rval=1
+  fi
+  return ${rval}
+}
+
 # check for IP via dns resolution
 echo -n 'Checking for VPN IP... '
 ip=$(dig @resolver1.opendns.com myip.opendns.com +short) || { echo "dig command failed, exiting."; exit 1; }
@@ -13,19 +59,13 @@ output=$(/bin/geoiplookup "${ip}") || { echo 'geoiplookup command failed, exitin
 echo "found: ${output}"
 
 # check to make sure we're resolving from Canadia... for reasons
-# TODO: make this more readable
-#[[ "${output}" =~ "Canada" ]] && {
-#  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
-#  [ ${output} -eq 0 ] && {
-#    echo "Did not find running transmission-daemon, starting"
-#    systemctl start transmission-daemon;
-#    logger transmission-killswitch "Starting transmission as openvpn is running just fine";
-#  } || { echo "Transmission running, keep on keeping on"; }
-#} || {
-#  output=$(ps aux | grep -i transmission | grep -v grep | grep -v killswitch | wc -l)
-#  [ ${output} -ne 0 ] && {
-#    echo "Found running transmission-daemon, stopping"
-#    systemctl stop transmission-daemon;
-#    logger transmission-killswitch "Stopping transmission as openvpn is not running right";
-#  } || { echo "Transmission dead, all is well"; }
-#}
+echo -n "Ensuring we're pinned to '${wanted_country}'... "
+
+# if we're not, then we want to stop transmission.
+if [[ "${output}" =~ "Canada" ]]; then
+  echo "look at that. Fancy. Let's make sure things are moving along."
+  start_transmission
+else
+  echo "oops, guess not. Let's stop the downloader, I guess."
+  stop_transmission
+fi
